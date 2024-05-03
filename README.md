c9410f10f3b345a4468b1c788343a1f7776166e2
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2024-05-03 13:41:42 +0100

    windows file

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ to shring all videos and audios to their 1s versions:
 - move folder called software_validation_tools to the cloned folder
 - run the script in terminal in folder software_validation_tools with
 
-`./shrink_videos_and_audios.sh`
+`./shrink_videos_and_audios.sh` on mac
+`script_name.bat` on windows
 
 - new folder called `__pool_short__` will be created, check if everything is ok in it
 - delete the old `__pool__` folder and rename `__pool_short__` to now be called `__pool__`
---
+++ software_validation_tools/shrink_videos_and_audios.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Directory containing the original mp4 files
+set "SOURCE_DIR=..\__pool__"
+
+rem Directory to save the shortened mp4 files
+set "DEST_DIR=..\__pool_short__"
+
+rem Create the destination directory if it doesn't exist
+if not exist "%DEST_DIR%" mkdir "%DEST_DIR%"
+
+rem Find all video files in the source directory and its subdirectories
+for /r "%SOURCE_DIR%" %%F in (*.avi) do (
+    rem Compute the path of the new file by replacing the source directory with the destination directory
+    set "FILE=%%F"
+    set "NEW_FILE=!FILE:%SOURCE_DIR%=%DEST_DIR%!"
+
+    rem Create the directory structure for the new file
+    mkdir "%~dpNEW_FILE%" 2>nul
+
+    rem Use ffmpeg to create a 1-second clip from the original video
+    ffmpeg -y -i "!FILE!" -ss 0 -t 1 -c:v copy -c:a copy "!NEW_FILE!"
+)
+
+rem Find all image files in the source directory and its subdirectories
+for /r "%SOURCE_DIR%" %%F in (*.jpg) do (
+    set "FILE=%%F"
+    set "NEW_FILE=!FILE:%SOURCE_DIR%=%DEST_DIR%!"
+    mkdir "%~dpNEW_FILE%" 2>nul
+    copy "!FILE!" "!NEW_FILE!"
+)
+
+rem Find all audio files in the source directory and its subdirectories
+for /r "%SOURCE_DIR%" %%F in (*.wav) do (
+    set "FILE=%%F"
+    set "NEW_FILE=!FILE:%SOURCE_DIR%=%DEST_DIR%!"
+    mkdir "%~dpNEW_FILE%" 2>nul
+    ffmpeg -y -i "!FILE!" -ss 0 -t 1 -c:a copy "!NEW_FILE!"
+)
